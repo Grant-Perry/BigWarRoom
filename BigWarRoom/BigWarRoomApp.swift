7815e1f16046ff128cb8338b264796f17f8e5b8e
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2025-08-25 13:30:31 -0400

    Initial Commit

---
+++ BigWarRoom/BigWarRoomApp.swift
@@ -0,0 +1,19 @@
+// BigWarRoom --> BigWarRoomApp.swift
+//  
+//   Created by: Gp. on 8/25/25 at 1:30 PM
+//     Modified: 
+//  Copyright © 2025 Cre8vPlanet Studios, LLC. - Grant Perry - all rights reserved.
+
+import SwiftUI
+
+
+import SwiftUI
+
+@main
+struct BigWarRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigWarRoom/ContentView.swift
@@ -0,0 +1,26 @@
+// BigWarRoom --> ContentView.swift
+//  
+//   Created by: Gp. on 8/25/25 at 1:30 PM
+//     Modified: 
+//  Copyright © 2025 Cre8vPlanet Studios, LLC. - Grant Perry - all rights reserved.
+
+import SwiftUI
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
